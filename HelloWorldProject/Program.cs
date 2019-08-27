1ad7091f8a9143f0ed2491df895cc485ed826628
max-student@outlook.com <max-student@outlook.com>
2019-08-27 15:21:25 -0400

    final commit

--- HelloWorldProject/Program.cs
+++ HelloWorldProject/Program.cs
@@ -4,15 +4,12 @@ using System;
 
 namespace HelloWorldProject
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    class Program {
+        static void Main(string[] args) {
             Console.Write("Hello C# Boot Camp World!");
             Console.WriteLine("At Max Technical Training");
             int daysInBootcamp = 66;
-            if (daysInBootcamp == 6)
-            {
+            if (daysInBootcamp == 6) {
                 Console.WriteLine("Yes");
             }
             else {
@@ -31,7 +28,7 @@ namespace HelloWorldProject
             var ints = new int[] { 1, 2, 3, 4, 5 };
             var total = 0;
             var index = 0;
-            while(index < 5) {
+            while (index < 5) {
                 total = total + (ints[index] * ints[index]);
                 index = index + 1;
             }
@@ -49,25 +46,50 @@ namespace HelloWorldProject
             var scores = new int[] { 738, 609, 307, 959, 805, 689, 402, 972, 359, 140 };
             #region assignment compute the average
             var tot = 0;
-            for (var idx = 0; idx < scores.Length; idx = idx +1) {
+            for (var idx = 0; idx < scores.Length; idx = idx + 1) {
                 tot = tot + scores[idx];
             }
             var avg = tot / scores.Length;
             Console.WriteLine("Average is " + avg);
-            #endregion 
+            #endregion
 
-            
+
             //assignment compute the average
 
-                // looping with foreach()
+            // looping with foreach()
 
             var grandTotal = 0;
-            foreach(var score in scores) {
+            foreach (var score in scores) {
                 grandTotal += score;
             }
             Console.WriteLine("GrandTotal is " + grandTotal);
 
+
+            // floating point numbers
+            var totaldistances = 0.0;
+            var distances = new double[] { 5.99, 3.56, 8.99, 7.47, 8.17, 6.78, 6.33 };
+            foreach (var distance in distances) {
+                totaldistances += distance;
+            }
+            var avgdistance = totaldistances / distances.Length;
+
+            // arrays
+            int[] students = new int[5];
+            students[0] = 7;
+            students[1] = 3;
+            students[2] = 4;
+            students[3] = 15;
+            students[4] = 5;
+            
+
+            string[] names = new string[7];
+            names[0] = "Jeremy";
+            names[1] = "Alex";
+            
+
         }
+       
+ 
     }
     
 }
